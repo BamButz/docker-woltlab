94c71fd1be3d3cf73119c1c7f63f100aaddd06b8
BamButz <bambutz@cryptonica.de>
2020-09-04 16:53:41 +0200

    fixed if statement to only copy if directory empty

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ service nginx start
 WOLTLAB_SOURCE=/opt/woltlab
 HTML_ROOT=/var/www/woltlab
 
-if [ "$(ls -A $HTML_ROOT)" ]; then
+if ! [ "$(ls -A $HTML_ROOT)" ]; then
     echo "Copy source files to web root"
     mv $WOLTLAB_SOURCE/* $HTML_ROOT
     chown -R www-data:www-data $HTML_ROOT
